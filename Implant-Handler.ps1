2a065940da6790bddda78d9052a18a18a23be9e0
Ben Turner <benpturner@yahoo.com>
2017-09-28 20:07:02 +0100

    Updated Alias Powerup

--- Implant-Handler.ps1
+++ Implant-Handler.ps1
@@ -1313,6 +1313,10 @@ param
             { 
                 CheckModuleLoaded "Powerup.ps1" $psrandomuri
             }
+            if ($pscommand.ToLower().StartsWith('invoke-privescaudit'))
+            { 
+                CheckModuleLoaded "Powerup.ps1" $psrandomuri
+            }
             if ($pscommand.ToLower().StartsWith('invoke-wscriptuacbypass'))
             { 
                 CheckModuleLoaded "Invoke-WScriptBypassUAC.ps1" $psrandomuri
